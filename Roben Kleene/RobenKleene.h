a066118cf10cc9931fbaca8970a914c32f91d694
Roben Kleene <robenk@gmail.com>
2016-02-18 20:21:52 -0500

    Setup Copy Files

---
+++ Roben Kleene/RobenKleene.h
@@ -0,0 +1,13 @@
+//
+//  RobenKleene.h
+//  Roben Kleene
+//
+//  Created by Roben Kleene on 2/18/16.
+//  Copyright © 2016 Roben Kleene. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RobenKleene : NSObject
+
+@end
---
+++ Roben Kleene/RobenKleene.m
@@ -0,0 +1,17 @@
+//
+//  RobenKleene.m
+//  Roben Kleene
+//
+//  Created by Roben Kleene on 2/18/16.
+//  Copyright © 2016 Roben Kleene. All rights reserved.
+//
+
+#import "RobenKleene.h"
+
+@implementation RobenKleene
+
++ (void)load {
+    NSLog(@"RobenKleene loaded");
+}
+
+@end
