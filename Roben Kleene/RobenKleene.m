e260cc816462d69d414d29f20f02d2e93ed0499a
Roben Kleene <robenk@gmail.com>
2016-02-25 20:24:45 -0500

    Add commented out menu item

--- Roben Kleene/RobenKleene.m
+++ Roben Kleene/RobenKleene.m
@@ -8,10 +8,42 @@
 
 #import "RobenKleene.h"
 
+#import <AppKit/AppKit.h>
+
 @implementation RobenKleene
 
 + (void)load {
+
+//    NSMenuItem *editMenuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
+//  
+//    if (!editMenuItem) {
+//        NSLog(@"Returning");
+//
+//        return;
+//    }
+//    
+//    // Create the delete menu item
+//    NSMenuItem *deleteLineMenuItem = [[NSMenuItem alloc] initWithTitle:@"Delete Line"
+//                                                                action:@selector(deleteLine:)
+//                                                         keyEquivalent:@"d"];
+//    [deleteLineMenuItem setTarget:self];
+//    [deleteLineMenuItem setKeyEquivalentModifierMask:NSShiftKeyMask | NSControlKeyMask];
+//
+//    // Create the "Roben Kleene" menu item
+//    NSMenuItem *robenKleeneMenuItem = [[NSMenuItem alloc] init];
+//    NSMenu *robenKleeneMenu = [[NSMenu alloc] init];
+//    [robenKleeneMenu addItem:deleteLineMenuItem];
+//    [robenKleeneMenuItem setSubmenu:robenKleeneMenu];
+//    
+//    // Add to the menu
+//    [[editMenuItem submenu] addItem:[NSMenuItem separatorItem]];
+//    [[editMenuItem submenu] addItem:robenKleeneMenuItem];
+
     NSLog(@"RobenKleene.xcplugin Loaded");
+}
+
+- (void)deleteLine:(id)sender {
+    NSLog(@"Got here");
 }
 
 @end
